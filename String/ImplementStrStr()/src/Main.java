dcb293fe72ee59ce940aec81f0198b499d0ce660
ZhangXiaoM <zhangmc81@163.com>
2018-01-19 10:25:20 +0800

    Implement strStr()

---
+++ String/ImplementStrStr()/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("");
+    }
+
+
+    public static int strStr(String haystack, String needle) {
+        int res = haystack.indexOf(needle);
+        return res;
+    }
+}
