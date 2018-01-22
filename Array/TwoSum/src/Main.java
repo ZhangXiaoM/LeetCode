7fe62505fb57430687d7f93607b17bfed1a8cefe
ZhangXiaoM <zhangmc81@163.com>
2018-01-22 18:16:07 +0800

    Two Sum

--- Array/TwoSum/src/Main.java
+++ Array/TwoSum/src/Main.java
@@ -1,3 +1,16 @@
+
+/*
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+
+ */
+
 import java.lang.reflect.Array;
 
 import java.util.Arrays;
@@ -6,10 +19,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        int[] ints = {3,2,4};
-        System.out.println(Main.twoSum(ints, 6));
+        int[] ints = {3,2,4,4,7,20,7};
+        System.out.println(twoSum2(ints, 8));
     }
 
+    // time complexity O(N^2)
     public static int[] twoSum(int[] nums, int target) {
 
         int[] re = {0,0};
@@ -24,5 +38,18 @@ public class Main {
         }
 
         return re;
+    }
+
+    // time complexity O(N*lgN)
+    public static int[] twoSum2(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length - 1; ++i) {
+            int j = Arrays.binarySearch(numbers, i + 1, numbers.length, target - numbers[i]);
+            if (j >= 0) {
+                int[] res = {i + 1, j + 1};
+                return res;
+            }
+        }
+
+        return new int[0];
     }
 }
