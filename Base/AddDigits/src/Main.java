8c28116fb02d3f7f4460b375de94886ef2bcc83b
ZhangXiaoM <zhangmc81@163.com>
2018-02-08 16:08:08 +0800

    add digital

---
+++ Base/AddDigits/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static int addDigits(int num) {
+        return num == 0? 0 : (num % 9 == 0? 9 : num % 9);
+    }
+}
