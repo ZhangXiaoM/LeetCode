ee5b76426fadcf321f4eb10e261e048bf112fb65
ZhangXiaoM <zhangmc81@163.com>
2018-02-09 17:01:02 +0800

    Palindrome Linked List creat

---
+++ LinkList/PalindromeLinkedList/src/Main.java
@@ -0,0 +1,29 @@
+
+/*
+
+https://leetcode.com/problems/palindrome-linked-list/description/
+
+Given a singly linked list, determine if it is a palindrome.
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    private class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        
+    }
+}
