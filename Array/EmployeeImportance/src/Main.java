e2b04ca26af526ac9eb87c28e31e632f820bb739
ZhangXiaoM <zhangmc81@163.com>
2018-02-22 16:13:30 +0800

    Employee Importance Link

--- Array/EmployeeImportance/src/Main.java
+++ Array/EmployeeImportance/src/Main.java
@@ -1,7 +1,7 @@
 
 /*
 
-
+https://leetcode.com/problems/employee-importance/description/
 
 You are given a data structure of employee information, which includes the employee's unique id, his importance value and his direct subordinates' id.
 
