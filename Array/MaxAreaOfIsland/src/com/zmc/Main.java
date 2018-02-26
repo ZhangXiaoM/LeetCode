51ace2c22e443bfdec24c6f54bc163aa0da7a544
ZhangXiaoM <zhangmc81@163.com>
2018-02-26 16:35:38 +0800

    Max Area of Island

---
+++ Array/MaxAreaOfIsland/src/com/zmc/Main.java
@@ -0,0 +1,71 @@
+
+/*
+
+Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.)
+You may assume all four edges of the grid are surrounded by water.
+
+Find the maximum area of an island in the given 2D array.
+(If there is no island, the maximum area is 0.)
+
+Example 1:
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+
+Given the above grid, return 6. Note the answer is not 11,
+because the island must be connected 4-directionally.
+Example 2:
+[[0,0,0,0,0,0,0,0]]
+Given the above grid, return 0.
+Note: The length of each dimension in the given grid does not exceed 50.
+
+
+ */
+
+
+package com.zmc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] grid = {{0,0,1,0,0,0,0,1,0,0,0,0,0},{0,0,0,0,0,0,0,1,1,1,0,0,0},{0,1,1,0,1,0,0,0,0,0,0,0,0},{0,1,0,0,1,1,0,0,1,0,1,0,0},
+                {0,1,0,0,1,1,0,0,1,1,1,0,0},{0,0,0,0,0,0,0,0,0,0,1,0,0},{0,0,0,0,0,0,0,1,1,1,0,0,0},{0,0,0,0,0,0,0,1,1,0,0,0,0}};
+        System.out.print(maxAreaOfIsland(grid));
+    }
+
+    public static int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+
+        int row = grid.length, col = grid[0].length;
+        int max = 0;
+
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (grid[i][j] == 1) {
+                    int cnt = countOfAreas(grid, i, j, row, col);
+                    max = Math.max(max, cnt);
+                }
+            }
+        }
+
+        return max;
+    }
+
+
+    private static int countOfAreas(int[][] grid, int i, int j, int row, int col) {
+        if (i < 0 || i >= row || j < 0 || j >= col || grid[i][j] == 0) return 0;
+        grid[i][j] = 0;
+        int cnt = 1;
+        cnt += countOfAreas(grid,i + 1, j, row, col);
+        cnt += countOfAreas(grid,i, j + 1, row, col);
+        cnt += countOfAreas(grid,i - 1, j, row, col);
+        cnt += countOfAreas(grid,i, j - 1, row, col);
+
+        return cnt;
+    }
+}
