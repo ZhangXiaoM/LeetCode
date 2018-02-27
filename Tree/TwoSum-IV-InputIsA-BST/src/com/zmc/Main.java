f6623d90824f5573413dde3705c8032957f2fa7d
ZhangXiaoM <zhangmc81@163.com>
2018-02-27 16:16:54 +0800

    Two Sum IV - Input is a BST

---
+++ Tree/TwoSum-IV-InputIsA-BST/src/com/zmc/Main.java
@@ -0,0 +1,78 @@
+
+/*
+
+https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
+
+Given a Binary Search Tree and a target number,
+return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+Example 1:
+Input:
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 9
+
+Output: True
+Example 2:
+Input:
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 28
+
+Output: False
+
+ */
+
+package com.zmc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
+        if (root == null) return false;
+
+        List<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            if (list.get(i) + list.get(j) == k) return true;
+            if (list.get(i) + list.get(j) < k) {
+                i ++;
+            } else {
+                j --;
+            }
+        }
+
+        return false;
+    }
+
+    void inOrder(TreeNode root, List list) {
+        if (root == null) return;
+
+        inOrder(root.left, list);
+        list.add(root.val);
+        inOrder(root.right, list);
+    }
+}
