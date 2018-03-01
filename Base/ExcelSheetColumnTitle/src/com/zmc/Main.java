152931c5b5e0aba2cced5f3a9486048193e01e94
ZhangXiaoM <zhangmc81@163.com>
2018-03-01 16:40:39 +0800

    Excel Sheet Column Title description

--- Base/ExcelSheetColumnTitle/src/com/zmc/Main.java
+++ Base/ExcelSheetColumnTitle/src/com/zmc/Main.java
@@ -1,3 +1,23 @@
+
+/*
+
+https://leetcode.com/problems/excel-sheet-column-title/description/
+
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB
+
+ */
+
+
 package com.zmc;
 
 public class Main {
