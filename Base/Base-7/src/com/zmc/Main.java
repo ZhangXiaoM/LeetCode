2a0c54ff17771c9a91403d5160342a33f3abe4cf
Zhang Mingchen <zhangmc81@163.com>
2018-03-05 16:22:34 +0800

    Base 7

---
+++ Base/Base-7/src/com/zmc/Main.java
@@ -0,0 +1,32 @@
+package com.zmc;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print(convertToBase7(0));
+    }
+
+    public static String convertToBase7(int num) {
+
+        if (num == 0) return "0";
+        
+        String res = "";
+        int val = num;
+        while (val != 0) {
+            int temp = Math.abs(val % 7);
+            res += String.valueOf(temp);
+            val /= 7;
+        }
+        res += num < 0? "-" : "";
+
+        char[] nums = res.toCharArray();
+        for (int i = 0; i < nums.length / 2; ++i) {
+            char temp = nums[i];
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = temp;
+        }
+
+        return new String(nums);
+    }
+}
