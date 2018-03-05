9e0e3067baacedcb4c97b5d4cbaa55e4010b0d66
Zhang Mingchen <zhangmc81@163.com>
2018-03-05 16:23:03 +0800

    Base 7 description

--- Base/Base-7/src/com/zmc/Main.java
+++ Base/Base-7/src/com/zmc/Main.java
@@ -1,3 +1,20 @@
+
+/*
+
+https://leetcode.com/problems/base-7/description/
+
+Given an integer, return its base 7 string representation.
+
+Example 1:
+Input: 100
+Output: "202"
+Example 2:
+Input: -7
+Output: "-10"
+Note: The input will be in range of [-1e7, 1e7].
+
+ */
+
 package com.zmc;
 
 public class Main {
@@ -10,7 +27,7 @@ public class Main {
     public static String convertToBase7(int num) {
 
         if (num == 0) return "0";
-        
+
         String res = "";
         int val = num;
         while (val != 0) {
