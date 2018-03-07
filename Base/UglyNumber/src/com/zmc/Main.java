74685f1da54f203440e51bc657342f61876a6699
Zhang Mingchen <zhangmc81@163.com>
2018-03-07 15:09:12 +0800

    ugly number description

--- Base/UglyNumber/src/com/zmc/Main.java
+++ Base/UglyNumber/src/com/zmc/Main.java
@@ -1,3 +1,17 @@
+
+/*
+
+https://leetcode.com/problems/ugly-number/description/
+
+Write a program to check whether a given number is an ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+
+Note that 1 is typically treated as an ugly number.
+
+ */
+
 package com.zmc;
 
 public class Main {
