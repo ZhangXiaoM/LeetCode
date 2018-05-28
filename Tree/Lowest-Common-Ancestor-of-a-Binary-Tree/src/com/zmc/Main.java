f53df7a16979181c953c583c1135c2d2c50fd8ef
Zhang Mingchen <zhangmc81@163.com>
2018-05-28 18:31:25 +0800

    Lowest-Common-Ancestor-of-a-Binary-Tree

---
+++ Tree/Lowest-Common-Ancestor-of-a-Binary-Tree/src/com/zmc/Main.java
@@ -0,0 +1,69 @@
+
+/*
+
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia:
+“The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants
+(where we allow a node to be a descendant of itself).”
+
+Given the following binary search tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+
+        _______3______
+       /              \
+    ___5__          ___1__
+   /      \        /      \
+   6      _2       0       8
+         /  \
+         7   4
+Example 1:
+
+Input: root, p = 5, q = 1
+Output: 3
+Explanation: The LCA of of nodes 5 and 1 is 3.
+Example 2:
+
+Input: root, p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself
+             according to the LCA definition.
+
+ */
+
+
+package com.zmc;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+        TreeNode res = lowestCommonAncestor(root, new TreeNode(3), new TreeNode(9));
+        System.out.print(res.val);
+    }
+
+    // just three case:
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+}
