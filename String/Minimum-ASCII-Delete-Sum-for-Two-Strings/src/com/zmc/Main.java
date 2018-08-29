c1eb9a5b8daf4682e16d5c5524f6768dce7f51d8
zhangmingchen <zhangmc81@163.com>
2018-08-29 16:16:01 +0800

    min ascii delete sum of two strings

--- String/Minimum-ASCII-Delete-Sum-for-Two-Strings/src/com/zmc/Main.java
+++ String/Minimum-ASCII-Delete-Sum-for-Two-Strings/src/com/zmc/Main.java
@@ -1,8 +1,61 @@
+/**
+ *
+ * https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/description/
+ *
+ * Given two strings s1, s2, find the lowest ASCII sum of deleted characters to make two strings equal.
+ *
+ * Example 1:
+ * Input: s1 = "sea", s2 = "eat"
+ * Output: 231
+ * Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the sum.
+ * Deleting "t" from "eat" adds 116 to the sum.
+ * At the end, both strings are equal, and 115 + 116 = 231 is the minimum sum possible to achieve this.
+ * Example 2:
+ * Input: s1 = "delete", s2 = "leet"
+ * Output: 403
+ * Explanation: Deleting "dee" from "delete" to turn the string into "let",
+ * adds 100[d]+101[e]+101[e] to the sum.  Deleting "e" from "leet" adds 101[e] to the sum.
+ * At the end, both strings are equal to "let", and the answer is 100+101+101+101 = 403.
+ * If instead we turned both strings into "lee" or "eet", we would get answers of 433 or 417, which are higher.
+ * Note:
+ *
+ * 0 < s1.length, s2.length <= 1000.
+ * All elements of each string will have an ASCII value in [97, 122].
+ */
+
 package com.zmc;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.print(minimumDeleteSum("delete", "leet"));
+    }
+
+    public static int minimumDeleteSum(String s1, String s2) {
+
+        int sum = 0, m = s1.length(), n = s2.length();
+
+        for (char c: s1.toCharArray()) {
+            sum += c;
+        }
+
+        for (char c: s2.toCharArray()) {
+            sum += c;
+        }
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + s1.charAt(i - 1) * 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return sum - dp[m][n];
     }
 }
