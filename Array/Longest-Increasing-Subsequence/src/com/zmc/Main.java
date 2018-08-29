b180b993ac7b20bc30270161b036ef2f38097496
zhangmingchen <zhangmc81@163.com>
2018-08-29 17:02:38 +0800

    LIS-

--- Array/Longest-Increasing-Subsequence/src/com/zmc/Main.java
+++ Array/Longest-Increasing-Subsequence/src/com/zmc/Main.java
@@ -34,7 +34,7 @@ public class Main {
         for (int i = 0; i < n; ++i) {
             dp[i] = 1;
         }
-
+        
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
                 if (nums[i] > nums[j])
